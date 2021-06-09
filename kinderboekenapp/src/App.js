9578dd775313235290b3b8c77628f03dcf4c1784
Baeshee <basbarg@gmail.com>
2021-06-09 13:51:03 +0200

    Navbar met Router import
    Navbar met doorlinking naar de verschillende pagina's en Router (import) om de verschillende routes op een one page website af te handelen.

--- kinderboekenapp/src/App.js
+++ kinderboekenapp/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+  } from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './Navbar';
+import { NavItems } from './NavItems';
+
+class App extends Component {
+    render() {
+        return (
+            <main>
+                <Router>
+                    <Route path="/boekenplank">
+                        <h1>Boekenplank</h1>
+                    </Route>
+                    <Route path="/boekenlijst">
+                        <h1>Boekenlijst</h1>
+                    </Route>
+                    <Route path="/logout">
+                        <h1>Logout</h1>
+                    </Route><Route path="/moppentrommel">
+                        <h1>Moppentrommel</h1>
+                    </Route>
+                    <Route path="/profiel">
+                        <h1>Profiel</h1>
+                    </Route>
+                </Router>
+                <Navbar navitems={NavItems} />
+            </main>
+        )
+    }
 }
 
 export default App;
---
+++ kinderboekenapp/src/NavItems.js
@@ -0,0 +1,27 @@
+export const NavItems = [
+    {
+        cName: 'nav__list__link',
+        url: '/boekenplank',
+        icon: 'fas fa-book fa-2x'
+    },
+    {
+        cName: 'nav__list__link',
+        url: '/boekenlijst',
+        icon: 'fas fa-bookmark fa-2x'
+    },
+    {
+        cName: 'nav__list__link',
+        url: '/moppentrommel',
+        icon: 'fas fa-comment-dots fa-2x'
+    },
+    {
+        cName: 'nav__list__link',
+        url: '/profiel',
+        icon: 'fas fa-user-alt fa-2x'
+    },
+    {
+        cName: 'nav__list__link',
+        url: '/logout',
+        icon: 'fas fa-sign-out-alt fa-2x'
+    },
+]
