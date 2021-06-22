38cba221043de30094f5dada5cc3e8f69d9d61fd
Dishang <dishang04@gmail.com>
2021-06-22 15:17:41 +0200

    searchbar works when empty again and add button from booklist

--- kinderboekenapp/src/BookCard.js
+++ kinderboekenapp/src/BookCard.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import './BookCard.css';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import axios from 'axios';
 
-const BookCard = (props) => {
-    return(
-        <article className="bookCard">
-            <figure>
-                <img className="bookCard__img" src={props.img || "/img/botje.jpg"} alt="Foto van een boek"/>
-            </figure> 
-            <section >
-                <h2 className="bookCard__title">{props.bookTitle || "Boek naam"}</h2>
-                <p className="bookCard__info">
-                    Auteur: {props.writer || "naam"}
-                    <br/>
-                    Genre: {props.genre || "genre"}
-                </p>
-             </section>
-            <button className="bookCard__addButton">+</button>
-            {/* <button>Klik voor meer informatie</button> */}
-        </article>
-    )
+
+class BookCard extends React.Component {
+    makeApiCall = addBook =>{                           //api post request to add the book to the bookshelf
+        console.log("apiCall " + addBook);
+        const BASE_URL = "https://kinderboekenapp-laravel.herokuapp.com/api/booktouser/";
+        axios.post(BASE_URL, addBook);
+    }
+
+    onAddClicked = () =>{                           //Pressed on the add book to bookshelf gets the id 
+        console.log(this.props.id);
+        this.makeApiCall(this.props.id);
+    }
+
+    
+    render(){  
+        return(                                     //the card with all the book information
+            <article className="bookCard">
+                <figure>
+                    <img className="bookCard__img" src={this.props.img || "/img/botje.jpg"} alt="Foto van een boek"/>
+                </figure> 
+                <section >
+                    <h2 className="bookCard__title">{this.props.bookTitle || "Boek naam"}</h2>
+                    <p className="bookCard__info">
+                        Auteur: {this.props.writer || "naam"}
+                        <br/>
+                        Genre: {this.props.genre || "genre"}
+                    </p>
+                 </section>
+                <button className="bookCard__addButton" onClick={this.onAddClicked}>+</button>
+            </article>
+        );
+    }
 }
 
 export default BookCard;
--- kinderboekenapp/src/Searchbar.js
+++ kinderboekenapp/src/Searchbar.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import './Searchbar.css';
-
 import axios from 'axios';
 import {connect} from "react-redux";
 import { changeSearchTerm, changeBooks} from "./actions.js";
 
 class Searchbar extends React.Component{
 
-    // state = {searchTerm: ""};
-
-    componentDidMount(){
+    componentDidMount(){            //api call to load and show all the books
         this.makeApiCall("");
     }
 
-    makeApiCall = (searchTerm) =>{
-        const BASE_URL = "https://kinderboekenapp-laravel.herokuapp.com/api/books/";
-        axios.get(BASE_URL + searchTerm).then(res => {
+    makeApiCall = (searchTerm) =>{   
+        const BASE_URL = "https://kinderboekenapp-laravel.herokuapp.com/api/books";     
+        if(searchTerm === ""){               //if searchbar is empty show all books
+            console.log("empty");
+            axios.get(BASE_URL).then(res =>{
+                this.props.changeBooks(res.data);
+                const data = res.data;
+                console.log(data);   
+            });
+        }
+        else{
+            axios.get(BASE_URL + "/" + searchTerm).then(res => {            //searchbar is used
             this.props.changeBooks(res.data);
             const data = res.data;
             console.log(data);
-        })
+            });
+        }
     }
 
     onSearch = (event) =>{
-        this.props.changeSearchTerm(event.target.value);
+        this.props.changeSearchTerm(event.target.value);        //there is an input with a value
     }
 
-    onCheck = (event) =>{
+    onCheck = (event) =>{                                       //pressed enter to search for book
         event.preventDefault();
         this.makeApiCall(this.props.searchTerm);
     }
 
     render(){
         console.log(this.props.searchTerm);
-        return(
+        return(                                                         //the searchbar
             <section className="searchbar_section">
                 <form onSubmit={this.onCheck}>
                     <input onChange={this.onSearch} 
